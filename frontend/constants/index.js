6fcf5beac9b80bec49e68079987bbb68cf9f4c8e
yxtan02 <yxtan02@gmail.com>
2024-07-28 13:00:07 +0800

    Clean up code

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -1,5 +1,4 @@
-import { Colors, SIZES, SHADOWS } from './theme'
 import icons from "./icons";
 import images from "./images";
 
-export { icons, images, Colors, SIZES, SHADOWS }
+export { icons, images }
